9b2118259d1dee94dc5818da9f022bda8f6297c6
Camilaf19 <cam.fonseca19@gmail.com>
2022-09-07 11:09:46 -0500

    Se realiza enrutado con conflictos al refrescar la pagina. Vsersión video by Andres

--- src/lib/welcome.js
+++ src/lib/welcome.js
@@ -1,38 +1,63 @@
-export const welcome = () => { 
-  
-const rootDiv = document.getElementById("root");
-rootDiv.innerHTML = `<h1>Bienvenid@</h1> <a id = "login" href="#login">Ingresar</a>`;
+const loginContent = `<h2>Aqui pon tu correo</h2> 
+<a id="signIn" href="#signIn">Iniciar Sesion</a>
+<a href="#welcome">Regresar</a>`;
 
-let buttonLogin = document.getElementById("login");
-buttonLogin.addEventListener("hashchange", loginFunction);
 
-function loginFunction(){
- 
-    const pathname = window.location.pathname;
-    rootDiv.innerHTML = routes[pathname];
-    rootDiv.innerHTML = "";
+const signUpContent = `<h3>completa estos datos</h3> 
+<a id="signUpEnter" href="#signInEnter">Iniciar Sesion</a> <a href="#welcome">Regresar</a>`;
 
-// const linkContent = {
-//     "#welcome": welcome,
-//     "#loginEmail": loginEmail,
-// };
+export const welcome = () => {
 
-const routes = {
-"/login" : login,
+    const rootSection = document.getElementById("root");
+    rootSection.innerHTML = `<h1>Bienvenid@</h1> <a id="login" href="#login">Ingresar</a>
+ <a id="loginGoogle" href="#loginGoogle">Ingresar con Google</a>
+ <a id="signUp" href="#signUp">Registrate</a>
+`;
+
+
+    //cambiar HTML al clickear links href 
+    window.onhashchange = () => {
+        const hash = window.location.hash;
+        console.log(hash)
+        if (linkContent[hash] !== undefined) {
+            rootSection.innerHTML = linkContent[hash]
+            changeRoute(hash);
+        } else if (hash !== "") {
+            welcome();
+        }
+    };
+
+    const linkContent = {
+        "#login": loginContent,
+        "#signUp": signUpContent,
+    };
+    //cambiar ruta en la URL para cambiar el numeral (esto es a mano, no quiere decir que sea valida la ruta)
+     const changeRoute = (hash) => {
+     window.history.replaceState({}, "", routesUrl[hash]);
+    }
+
+    const routesUrl = {
+        "#login": "/login",
+        "#signUp": "/signUp",
+    };
+    //mostrar el html correcto al cargar la pagina 
+    const routes = {
+        "/login": loginContent,
+        "/signUp": signUpContent,
+    };
+
+    // const pathname = window.location.pathname;
+    //rootSection.innerHTML = routes[pathname];
+
+
+    //mostrar el HTML correcto al moverse atrás y adelante
+     window.onpopstate = () => {
+     const pathname = window.location.pathname;
+    if (routes[pathname] !== undefined) {
+     rootSection.innerHTML = routes[pathname];
+     } else {
+       welcome();
+     }
+ };
 };
-const changeRoute = (hash) => {
-    if (hash === "#login") {
-        window.history.replaceState({}, "login", "/");
-    }};
-// const pathname = window.location.pathname;
-// rootDiv.innerHTML = routes[pathname];
 
-// const changeRoute = (hash) => {
-//     if (hash === "#welcome") {
-//         window.history.replaceState({}, "welcome", "/");
-//     } else if (hash === "loginEmail"){
-//         window.history.replaceState({}, "loginEmail", "/login");
-//     }
-// }
-}
-};
--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@
 
 import  {welcome} from './lib/welcome.js';
 
-
-const rootDiv = document.getElementById("root");
-// rootDiv.innerHTML = `<h1>Bienvenid@</h1> <a href="#login">Ingresar</a>`;
-welcome(window.location.welcome);
+welcome();
 
