2e96f699e22de492ae3205a714434dbe62cc0c7f
Camilaf19 <cam.fonseca19@gmail.com>
2022-09-15 11:09:15 -0500

    Boton Google funciona correctamente al 100%. Comienzos de test con nuevo archivo config.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,16 +14,8 @@ import config from './config.js';
 // import { getFirestore } from 'firebase/firestore';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCwd_I9pnGtenncq4D3ADe2aqB2YwSqy54",
-    authDomain: "travelers-e8b5a.firebaseapp.com",
-    projectId: "travelers-e8b5a",
-    storageBucket: "travelers-e8b5a.appspot.com",
-    messagingSenderId: "532240998590",
-    appId: "1:532240998590:web:3ebe2040c59dda333dbeef",
-    measurementId: "G-KZ49QE41R7"
-}
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(config);
 
 const auth = getAuth();
 
